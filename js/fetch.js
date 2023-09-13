f17c258ce8a995faa00f89d9b7af352cbbd55fce
Jhony Vill <jhonyvill.dev@gmail.com>
2023-09-13 16:49:25 -0300

    feat/create interactions with javascript

---
+++ js/fetch.js
@@ -0,0 +1,15 @@
+const getData = async function getData() {
+  const path = "../data.json";
+  try {
+    const response = await fetch(path);
+
+    if (!response.ok) {
+      throw new Error(`status(${response.status} - ${response.statusText})`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.log(`Erro na busca de dados: ${error.stack}`);
+  }
+}
+
+export default getData;
